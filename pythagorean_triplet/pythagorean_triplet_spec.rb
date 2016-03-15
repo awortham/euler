83af0ced8c091005230347aaa0c58bb742bc4e2f
Aaron Wortham <wortham.aaron@gmail.com>
2016-03-15 09:10:09 -0600

    specs are passing now

--- pythagorean_triplet/pythagorean_triplet_spec.rb
+++ pythagorean_triplet/pythagorean_triplet_spec.rb
@@ -1,12 +1,12 @@
 require "./pythagorean_triplet"
 
 RSpec.describe PythagoreanTriplet do
-  it "finds the triplet which added together equals a number passed in" do
-    expect(PythagoreanTriplet.new.find_triplet_which_adds_to(12)).to eq 3
+  it "returns total product of triplet that equaled passed in number" do
+    expect(PythagoreanTriplet.new.find_triplet_which_adds_to(12)).to eq 60
   end
 
   it "works for larger numbers" do
-    expect(PythagoreanTriplet.new.find_triplet_which_adds_to(1000)).to eq 3
+    expect(PythagoreanTriplet.new.find_triplet_which_adds_to(1000)).to eq 31875000
   end
 
   it "finds factors for a number" do
@@ -15,9 +15,5 @@ RSpec.describe PythagoreanTriplet do
 
   it "creates a triplet" do
     expect(PythagoreanTriplet.new.create_triplet(6)).to eq [[7, 24, 25], [8, 15, 17], [9, 12, 15]]
-  end
-
-  it "totals the product of the triplet" do
-
   end
 end
